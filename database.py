c87a8bf576d3847c22128741e3f27b5cd179eae4
Salami Kabiru <kabirusalami41@gmail.com>
2023-11-02 13:58:53 +0100

    updated the database to remove secret

--- database.py
+++ database.py
@@ -3,4 +3,4 @@ from sqlalchemy import create_engine
 
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://ywthpovg1s6d7v4z53ka:pscale_pw_hVNACasjAxtKy6M98w58eiFegQidMjlBNvUkAzWaiAt@aws.connect.psdb.cloud:5432/practice_dev_db")
+TODO: connect to database
