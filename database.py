a3e1b008b1136618813cc21f9da16a73a347b624
Salami Kabiru <kabirusalami41@gmail.com>
2023-11-05 19:13:00 +0100

    added git ignore file

--- database.py
+++ database.py
@@ -1,6 +1,21 @@
-import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
 
-from sqlalchemy import create_engine
+load_dotenv()
+PASSWORD = os.getenv("DB_PASSWORD")
+USERNAME = os.getenv("DB_USERNAME")
+HOSTNAME = os.getenv("DB_HOST")
+DATABASE = os.getenv("DB_NAME")
 
-TODO: connect to database
+db_connection_string = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}/{DATABASE}?charset=utf8mb4"
+
+engine = create_engine(db_connection_string, connect_args={
+    "ssl": {
+        "ssl_ca": "/etc/ssl/cert.pem"
+    }
+})
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs"))
+    print(result.all())
