84508098723b1a8176af12874829bd5da258f2be
Salami Kabiru <kabirusalami41@gmail.com>
2023-11-08 15:42:34 +0100

    added the about template

--- app.py
+++ app.py
@@ -1,51 +1,61 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from forms import Registrationform, Loginform
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b409f3d6a632f93c0c1c6c99217ff1ff'
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Remote',
-        'salary': '$60,000'
-    },
-    {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Remote'
-    },
-    {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
-        'salary': '$65,000'
-    },
-    {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'Remote',
-        'salary': '$75,000'
-    }
-]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    
+
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html', jobs=JOBS)
+    return render_template('home.html')
 
-@app.route("/products.html")
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/products")
 def products():
     return render_template('products.html')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = Registrationform()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, password=form.password.data, email=form.email.data, phone=form.phone.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+        # return '<h1> new user has been created </h1>'
     return render_template('register.html', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form =  Loginform()
+    if form.validate_on_submit():
+        user = User.query.filter(text("email = :email")).params(email=form.email.data).first()        
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('products'))
+            else:
+                return '<h1> incorrect password, please try again! </h1>'
+        else:
+            return '<h1> this user does not exist </h1>'
     return render_template('login.html', form=form)
 
 
